989e3007bc897f5b8cf4dddda7a2dacdeb9e1568
iagocno <iagocno31@gmail.com>
2025-02-16 10:32:06 -0300

    Update testScript.lua

--- testScript.lua
+++ testScript.lua
@@ -14,7 +14,7 @@ local function playNotificationSound()
     notificationSound:Play()
 end
 
---TESTE PARA SE MUDOU O COD
+--TESTE PARA SE MUDOU O COD1
 
 local function sendNotification(title, text, duration)
         game.StarterGui:SetCore("SendNotification", {
